45d1f41f09ebf3e37ad500484a9ddcf7ecd82b8c
Chi Voong <Chi.voong@gmail.com>
2024-01-12 09:27:40 +0000

    turn off tests for now

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/whosthegirlin',function(){
+    return view('whosthegirlin');
+});
+
+Route::get('/whosthegirlin2',function(){
+    return view('whosthegirlin2');
+});
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
